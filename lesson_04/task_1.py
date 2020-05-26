06581ddf316f9e73ce8b7d16ff3c0411a63f0085
Dmitry Takmakov <dj-dat@yandex.ru>
2020-05-26 22:31:15 +0500

    small fixes

--- lesson_04/task_1.py
+++ lesson_04/task_1.py
@@ -33,14 +33,14 @@ def my_array_max(size):
 
 
 # функция готова, теперь пропускаем ее через таймит, увеличивая количество элементов массива в два раза в каждом тесте
-print(timeit.timeit('my_array_max(0, 100, 20)', number=10000, globals=globals()))  # 0.421552256000723
-print(timeit.timeit('my_array_max(0, 100, 40)', number=10000, globals=globals()))  # 0.9387782869998773
-print(timeit.timeit('my_array_max(0, 100, 80)', number=10000, globals=globals()))  # 2.753190261999407
-print(timeit.timeit('my_array_max(0, 100, 160)', number=10000, globals=globals()))  # 9.154586634000225
-print(timeit.timeit('my_array_max(0, 100, 320)', number=10000, globals=globals()))  # 33.02670952299923
-print(timeit.timeit('my_array_max(0, 100, 640)', number=10000, globals=globals()))  # 124.65933747099916
+print(timeit.timeit('my_array_max(20)', number=10000, globals=globals()))  # 0.421552256000723
+print(timeit.timeit('my_array_max(40)', number=10000, globals=globals()))  # 0.9387782869998773
+print(timeit.timeit('my_array_max(80)', number=10000, globals=globals()))  # 2.753190261999407
+print(timeit.timeit('my_array_max(160)', number=10000, globals=globals()))  # 9.154586634000225
+print(timeit.timeit('my_array_max(320)', number=10000, globals=globals()))  # 33.02670952299923
+print(timeit.timeit('my_array_max(640)', number=10000, globals=globals()))  # 124.65933747099916
 # тестируем с помощью сипрофайла с теми же условиями
-cProfile.run('my_array_max(0, 100, 20)')
+cProfile.run('my_array_max(20)')
 #         1    0.000    0.000    0.000    0.000 <string>:1(<module>)
 #        20    0.000    0.000    0.000    0.000 random.py:200(randrange)
 #        20    0.000    0.000    0.000    0.000 random.py:244(randint)
@@ -52,7 +52,7 @@ cProfile.run('my_array_max(0, 100, 20)')
 #        20    0.000    0.000    0.000    0.000 {method 'bit_length' of 'int' objects}
 #         1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
 #        21    0.000    0.000    0.000    0.000 {method 'getrandbits' of '_random.Random' objects}
-cProfile.run('my_array_max(0, 100, 40)')
+cProfile.run('my_array_max(40)')
 #         1    0.000    0.000    0.000    0.000 <string>:1(<module>)
 #        40    0.000    0.000    0.000    0.000 random.py:200(randrange)
 #        40    0.000    0.000    0.000    0.000 random.py:244(randint)
@@ -64,7 +64,7 @@ cProfile.run('my_array_max(0, 100, 40)')
 #        40    0.000    0.000    0.000    0.000 {method 'bit_length' of 'int' objects}
 #         1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
 #        52    0.000    0.000    0.000    0.000 {method 'getrandbits' of '_random.Random' objects}
-cProfile.run('my_array_max(0, 100, 80)')
+cProfile.run('my_array_max(80)')
 #         1    0.000    0.000    0.000    0.000 <string>:1(<module>)
 #        80    0.000    0.000    0.000    0.000 random.py:200(randrange)
 #        80    0.000    0.000    0.000    0.000 random.py:244(randint)
@@ -76,7 +76,7 @@ cProfile.run('my_array_max(0, 100, 80)')
 #        80    0.000    0.000    0.000    0.000 {method 'bit_length' of 'int' objects}
 #         1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
 #       100    0.000    0.000    0.000    0.000 {method 'getrandbits' of '_random.Random' objects}
-cProfile.run('my_array_max(0, 100, 160)')
+cProfile.run('my_array_max(160)')
 #         1    0.000    0.000    0.001    0.001 <string>:1(<module>)
 #       160    0.000    0.000    0.000    0.000 random.py:200(randrange)
 #       160    0.000    0.000    0.000    0.000 random.py:244(randint)
@@ -88,7 +88,7 @@ cProfile.run('my_array_max(0, 100, 160)')
 #       160    0.000    0.000    0.000    0.000 {method 'bit_length' of 'int' objects}
 #         1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
 #       201    0.000    0.000    0.000    0.000 {method 'getrandbits' of '_random.Random' objects}
-cProfile.run('my_array_max(0, 100, 320)')
+cProfile.run('my_array_max(320)')
 #         1    0.000    0.000    0.004    0.004 <string>:1(<module>)
 #       320    0.000    0.000    0.000    0.000 random.py:200(randrange)
 #       320    0.000    0.000    0.000    0.000 random.py:244(randint)
@@ -100,7 +100,7 @@ cProfile.run('my_array_max(0, 100, 320)')
 #       320    0.000    0.000    0.000    0.000 {method 'bit_length' of 'int' objects}
 #         1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
 #       408    0.000    0.000    0.000    0.000 {method 'getrandbits' of '_random.Random' objects}
-cProfile.run('my_array_max(0, 100, 640)')
+cProfile.run('my_array_max(640)')
 #         1    0.000    0.000    0.014    0.014 <string>:1(<module>)
 #       640    0.001    0.000    0.001    0.000 random.py:200(randrange)
 #       640    0.000    0.000    0.001    0.000 random.py:244(randint)
@@ -137,14 +137,14 @@ def teacher_array_max(size):
 
 
 # тестируем эту функцию с теми же условиями - увеличивая количество элементов в два раза с каждым тестом
-print(timeit.timeit('teacher_array_max(0, 100, 20)', number=10000, globals=globals()))  # 0.23247075200015388
-print(timeit.timeit('teacher_array_max(0, 100, 40)', number=10000, globals=globals()))  # 0.3897852869995404
-print(timeit.timeit('teacher_array_max(0, 100, 80)', number=10000, globals=globals()))  # 0.7419532499989145
-print(timeit.timeit('teacher_array_max(0, 100, 160)', number=10000, globals=globals()))  # 1.4201540630001546
-print(timeit.timeit('teacher_array_max(0, 100, 320)', number=10000, globals=globals()))  # 2.835826996999458
-print(timeit.timeit('teacher_array_max(0, 100, 640)', number=10000, globals=globals()))  # 5.672430847000214
+print(timeit.timeit('teacher_array_max(20)', number=10000, globals=globals()))  # 0.23247075200015388
+print(timeit.timeit('teacher_array_max(40)', number=10000, globals=globals()))  # 0.3897852869995404
+print(timeit.timeit('teacher_array_max(80)', number=10000, globals=globals()))  # 0.7419532499989145
+print(timeit.timeit('teacher_array_max(160)', number=10000, globals=globals()))  # 1.4201540630001546
+print(timeit.timeit('teacher_array_max(320)', number=10000, globals=globals()))  # 2.835826996999458
+print(timeit.timeit('teacher_array_max(640)', number=10000, globals=globals()))  # 5.672430847000214
 # шик, теперь - сипрофайл
-cProfile.run('teacher_array_max(0, 100, 20)')
+cProfile.run('teacher_array_max(20)')
 #         1    0.000    0.000    0.000    0.000 <string>:1(<module>)
 #        20    0.000    0.000    0.000    0.000 random.py:200(randrange)
 #        20    0.000    0.000    0.000    0.000 random.py:244(randint)
@@ -155,7 +155,7 @@ cProfile.run('teacher_array_max(0, 100, 20)')
 #        20    0.000    0.000    0.000    0.000 {method 'bit_length' of 'int' objects}
 #         1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
 #        22    0.000    0.000    0.000    0.000 {method 'getrandbits' of '_random.Random' objects}
-cProfile.run('teacher_array_max(0, 100, 40)')
+cProfile.run('teacher_array_max(40)')
 #         1    0.000    0.000    0.000    0.000 <string>:1(<module>)
 #        40    0.000    0.000    0.000    0.000 random.py:200(randrange)
 #        40    0.000    0.000    0.000    0.000 random.py:244(randint)
@@ -166,7 +166,7 @@ cProfile.run('teacher_array_max(0, 100, 40)')
 #        40    0.000    0.000    0.000    0.000 {method 'bit_length' of 'int' objects}
 #         1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
 #        49    0.000    0.000    0.000    0.000 {method 'getrandbits' of '_random.Random' objects}
-cProfile.run('teacher_array_max(0, 100, 80)')
+cProfile.run('teacher_array_max(80)')
 #         1    0.000    0.000    0.000    0.000 <string>:1(<module>)
 #        80    0.000    0.000    0.000    0.000 random.py:200(randrange)
 #        80    0.000    0.000    0.000    0.000 random.py:244(randint)
@@ -177,7 +177,7 @@ cProfile.run('teacher_array_max(0, 100, 80)')
 #        80    0.000    0.000    0.000    0.000 {method 'bit_length' of 'int' objects}
 #         1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
 #       103    0.000    0.000    0.000    0.000 {method 'getrandbits' of '_random.Random' objects}
-cProfile.run('teacher_array_max(0, 100, 160)')
+cProfile.run('teacher_array_max(160)')
 #         1    0.000    0.000    0.001    0.001 <string>:1(<module>)
 #       160    0.000    0.000    0.000    0.000 random.py:200(randrange)
 #       160    0.000    0.000    0.000    0.000 random.py:244(randint)
@@ -188,7 +188,7 @@ cProfile.run('teacher_array_max(0, 100, 160)')
 #       160    0.000    0.000    0.000    0.000 {method 'bit_length' of 'int' objects}
 #         1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
 #       221    0.000    0.000    0.000    0.000 {method 'getrandbits' of '_random.Random' objects}
-cProfile.run('teacher_array_max(0, 100, 320)')
+cProfile.run('teacher_array_max(320)')
 #         1    0.000    0.000    0.001    0.001 <string>:1(<module>)
 #       320    0.000    0.000    0.001    0.000 random.py:200(randrange)
 #       320    0.000    0.000    0.001    0.000 random.py:244(randint)
@@ -199,7 +199,7 @@ cProfile.run('teacher_array_max(0, 100, 320)')
 #       320    0.000    0.000    0.000    0.000 {method 'bit_length' of 'int' objects}
 #         1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
 #       393    0.000    0.000    0.000    0.000 {method 'getrandbits' of '_random.Random' objects}
-cProfile.run('teacher_array_max(0, 100, 640)')
+cProfile.run('teacher_array_max(640)')
 #         1    0.000    0.000    0.002    0.002 <string>:1(<module>)
 #       640    0.001    0.000    0.001    0.000 random.py:200(randrange)
 #       640    0.000    0.000    0.002    0.000 random.py:244(randint)
