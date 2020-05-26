f6f768bc4fed54f28323a2624098fe58e904e0c9
Dmitry Takmakov <dj-dat@yandex.ru>
2020-05-26 20:09:49 +0500

    task 1, iteration 1 with tests

--- lesson_04/task_1.py
+++ lesson_04/task_1.py
@@ -31,5 +31,47 @@ def my_array_max(min_value, max_value, size):
         return 'Все элементы данного массива встречаются только один раз.'
 
 
-# функция готова, теперь пропускаем ее через таймит
-print(timeit.timeit('my_array_max(0, 100, 20)', globals=globals()))
+# функция готова, теперь пропускаем ее через таймит, увеличивая количество элементов массива в два раза в каждом тесте
+print(timeit.timeit('my_array_max(0, 100, 20)', number=1000, globals=globals()))  # 0.0558772839995072
+print(timeit.timeit('my_array_max(0, 100, 40)', number=1000, globals=globals()))  # 0.12037088500073878
+print(timeit.timeit('my_array_max(0, 100, 80)', number=1000, globals=globals()))  # 0.3085587559999112
+print(timeit.timeit('my_array_max(0, 100, 160)', number=1000, globals=globals()))  # 1.0253302529999928
+print(timeit.timeit('my_array_max(0, 100, 320)', number=1000, globals=globals()))  # 3.4286873689998174
+print(timeit.timeit('my_array_max(0, 100, 640)', number=1000, globals=globals()))  # 12.684498782000446
+# тестируем с помощью сипрофайла с теми же условиями
+cProfile.run('my_array_max(0, 100, 20)')
+#        20    0.000    0.000    0.000    0.000 random.py:200(randrange)
+#        20    0.000    0.000    0.000    0.000 random.py:244(randint)
+#        20    0.000    0.000    0.000    0.000 random.py:250(_randbelow_with_getrandbits)
+#         1    0.000    0.000    0.000    0.000 task_1.py:10(<listcomp>)
+#         1    0.000    0.000    0.000    0.000 task_1.py:9(my_array_max)
+cProfile.run('my_array_max(0, 100, 40)')
+#        40    0.000    0.000    0.000    0.000 random.py:200(randrange)
+#        40    0.000    0.000    0.000    0.000 random.py:244(randint)
+#        40    0.000    0.000    0.000    0.000 random.py:250(_randbelow_with_getrandbits)
+#         1    0.000    0.000    0.000    0.000 task_1.py:10(<listcomp>)
+#         1    0.000    0.000    0.000    0.000 task_1.py:9(my_array_max)
+cProfile.run('my_array_max(0, 100, 80)')
+#        80    0.000    0.000    0.000    0.000 random.py:200(randrange)
+#        80    0.000    0.000    0.000    0.000 random.py:244(randint)
+#        80    0.000    0.000    0.000    0.000 random.py:250(_randbelow_with_getrandbits)
+#         1    0.000    0.000    0.000    0.000 task_1.py:10(<listcomp>)
+#         1    0.001    0.001    0.001    0.001 task_1.py:9(my_array_max)
+cProfile.run('my_array_max(0, 100, 160)')
+#       160    0.000    0.000    0.000    0.000 random.py:200(randrange)
+#       160    0.000    0.000    0.000    0.000 random.py:244(randint)
+#       160    0.000    0.000    0.000    0.000 random.py:250(_randbelow_with_getrandbits)
+#         1    0.000    0.000    0.001    0.001 task_1.py:10(<listcomp>)
+#         1    0.002    0.002    0.002    0.002 task_1.py:9(my_array_max)
+cProfile.run('my_array_max(0, 100, 320)')
+#       320    0.000    0.000    0.001    0.000 random.py:200(randrange)
+#       320    0.000    0.000    0.001    0.000 random.py:244(randint)
+#       320    0.000    0.000    0.000    0.000 random.py:250(_randbelow_with_getrandbits)
+#         1    0.000    0.000    0.001    0.001 task_1.py:10(<listcomp>)
+#         1    0.004    0.004    0.005    0.005 task_1.py:9(my_array_max)
+cProfile.run('my_array_max(0, 100, 640)')
+#       640    0.001    0.000    0.001    0.000 random.py:200(randrange)
+#       640    0.000    0.000    0.001    0.000 random.py:244(randint)
+#       640    0.000    0.000    0.001    0.000 random.py:250(_randbelow_with_getrandbits)
+#         1    0.000    0.000    0.002    0.002 task_1.py:10(<listcomp>)
+#         1    0.020    0.020    0.022    0.022 task_1.py:9(my_array_max)
